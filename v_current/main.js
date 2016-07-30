05464117256b39e8b716bd1a49677c27407da1f2
Jonathan <me@jonl.org>
2016-07-30 18:17:55 -0400

    prompt user for address to center p5 map on

--- v_current/main.js
+++ v_current/main.js
@@ -1,12 +1,10 @@
 var mapImage;
 
 function preload() {
-  // var address = prompt("What address would you like to center the map at?");
-  var address = "243 Crosshill Road wynnewood pa 19096"
+  var address = prompt("What address would you like to center the map at?");
   var encodedAddress = encodeURIComponent(address); // converts the string to valid URL characters
-                                                    // for example the space character becomes %20
-  var mapUrl = "http://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCSHUj37EscbQKeCSY4oyM1tCiIgRhEOTE&size=300x150&center=" + encodedAddress;
-  mapImage = loadImage(mapUrl);
+                                                    // for example the space character (" ") becomes "%20"
+  mapImage = loadImage("http://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCSHUj37EscbQKeCSY4oyM1tCiIgRhEOTE&size=300x150&center=" + encodedAddress);
 }
 
 function setup() {
