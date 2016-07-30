7b62745611093a7dafd7b1e12e8bf04db26c2be4
Jonathan <me@jonl.org>
2016-07-30 18:25:07 -0400

    refactor url construction and increase canvas size

--- v_current/main.js
+++ v_current/main.js
@@ -4,11 +4,18 @@ function preload() {
   var address = prompt("What address would you like to center the map at?");
   var encodedAddress = encodeURIComponent(address); // converts the string to valid URL characters
                                                     // for example the space character (" ") becomes "%20"
-  mapImage = loadImage("http://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCSHUj37EscbQKeCSY4oyM1tCiIgRhEOTE&size=300x150&center=" + encodedAddress);
+
+  var baseUrl = "http://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCSHUj37EscbQKeCSY4oyM1tCiIgRhEOTE"
+  var sizeParam = "&size=600x600";
+  var centerParam = "&center=" + encodedAddress;
+
+  var fullUrl = baseUrl + sizeParam + centerParam;
+  console.log(fullUrl);
+  mapImage = loadImage(fullUrl);
 }
 
 function setup() {
-  createCanvas(400, 200);
+  createCanvas(600, 600);
   background(240, 240, 240);
   image(mapImage, 0, 0);
 }
