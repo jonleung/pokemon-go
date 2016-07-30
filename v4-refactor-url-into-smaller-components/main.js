53092b6a064b342f63fa57b88e373fa289f05f80
Jonathan <me@jonl.org>
2016-07-30 18:26:57 -0400

    v4-refactor-url-into-smaller-components

---
+++ v4-refactor-url-into-smaller-components/main.js
@@ -0,0 +1,21 @@
+var mapImage;
+
+function preload() {
+  var address = prompt("What address would you like to center the map at?");
+  var encodedAddress = encodeURIComponent(address); // converts the string to valid URL characters
+                                                    // for example the space character (" ") becomes "%20"
+
+  var baseUrl = "http://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCSHUj37EscbQKeCSY4oyM1tCiIgRhEOTE"
+  var sizeParam = "&size=600x600";
+  var centerParam = "&center=" + encodedAddress;
+
+  var fullUrl = baseUrl + sizeParam + centerParam;
+  console.log(fullUrl);
+  mapImage = loadImage(fullUrl);
+}
+
+function setup() {
+  createCanvas(600, 600);
+  background(240, 240, 240);
+  image(mapImage, 0, 0);
+}
